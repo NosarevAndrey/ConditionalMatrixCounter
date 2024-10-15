8682e66ccb9fd738a0df838f68f28f0eece1126f
NosarevAndrey-p <nosarevandrey03@gmail.com>
2024-10-15 18:56:57 +0300

    Добавьте файлы проекта.

---
+++ ConditionalMatrixCounter/Matrix.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConditionalMatrixCounter
+{
+	public class Matrix
+	{
+		private double[,] data { get; }
+		private int rows { get; }
+		private int cols { get; }
+
+		public Matrix(double[,] data)
+		{
+			if (data.Length == 0) throw new ArgumentException("data array cant be empty");
+
+			rows = data.GetLength(0);
+			cols = data.GetLength(1);
+			if (rows == 0 || cols == 0) throw new ArgumentException("rows and cols must be greater than 0");
+
+			this.data = data;
+
+		}
+
+		public static Matrix Random(int n, int m, double fillMin = 0.0, double fillMax = 1.0)
+		{
+			if (n <= 0 || m <= 0) throw new ArgumentException("n and m must be greater than 0");
+
+			Random random = new();
+			double[,] data = new double[n, m];
+			for (int i = 0; i < n; i++)
+				for (int j = 0; j < m; j++)
+					data[i, j] = fillMin + random.NextDouble() * (fillMax - fillMin);
+			return new Matrix(data);
+		}
+
+		public int CountSatisfyCondition(Func<double, bool> callBack)
+		{
+			int counter = 0;
+			foreach (var number in data)
+				if (callBack(number)) counter++;
+			return counter;
+		}
+
+		private int CountSatisfyConditionBlocks(
+			Func<double, bool> callBack,
+			int currentThread, int totalThreads,
+			int totalBlocksRow, int totalBlocksCol, int blockSize)
+		{
+			int count = 0;
+
+			for (int blockIndex = currentThread; blockIndex < totalBlocksRow * totalBlocksCol; blockIndex += totalThreads)
+			{
+				int blockRow = blockIndex / totalBlocksCol;
+				int blockCol = blockIndex % totalBlocksCol;
+
+				for (int row = blockRow * blockSize; row < Math.Min((blockRow + 1) * blockSize, rows); row++)
+					for (int col = blockCol * blockSize; col < Math.Min((blockCol + 1) * blockSize, cols); col++)
+						if (callBack(data[row, col])) count++;
+			}
+			return count;
+		}
+		public int ThreadCountSatisfyCondition(Func<double, bool> callBack, int blockSize = 50, int? numOfThreads = null)
+		{
+			numOfThreads ??= Environment.ProcessorCount - 1;
+			int threadCount = (int)numOfThreads;
+
+			int totalBlocksRow = (int)Math.Ceiling((double)rows / blockSize);
+			int totalBlocksCol = (int)Math.Ceiling((double)cols / blockSize);
+
+			threadCount = Math.Min(threadCount, totalBlocksRow * totalBlocksCol);
+
+			Thread[] threads = new Thread[threadCount];
+			int[] results = new int[threadCount];
+
+			for (int i = 0; i < threadCount; i++)
+			{
+				int index = i;
+				threads[index] = new Thread(() =>
+				{
+					results[index] = CountSatisfyConditionBlocks(
+						callBack, index, threadCount, totalBlocksRow, totalBlocksCol, blockSize
+						);
+				});
+				threads[index].Start();
+			}
+
+			foreach (var thread in threads)
+				thread.Join();
+
+			int totalCount = 0;
+			foreach (var count in results)
+				totalCount += count;
+
+			return totalCount;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					sb.Append(data[i, j].ToString("0.00"));
+					if (j < cols - 1) sb.Append("\t");
+				}
+				sb.AppendLine();
+			}
+			return sb.ToString();
+		}
+		public static implicit operator string(Matrix matrix) => matrix.ToString();
+	}
+}
---
+++ ConditionalMatrixCounter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace ConditionalMatrixCounter
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+            static bool condition(double num) => num > 0.5;
+            SpeedTest(1000, 1000, condition, Environment.ProcessorCount - 1, 50, 10);
+			SpeedTest(1000, 1000, condition, 4, 50, 10);
+			SpeedTest(20000, 20000, condition, Environment.ProcessorCount - 1, 1000, 5);
+			SpeedTest(20000, 20000, condition, 4, 1000, 5);
+		}
+		static void SpeedTest(int n, int m, Func<double, bool> condition, int? threadCount, int blockSize, int numberOfTests)
+		{
+			Console.WriteLine($"Test for matrix {n}x{m}, with {threadCount} threads and block size {blockSize}x{blockSize}:");
+			Matrix matrix = Matrix.Random(n, m);
+			Stopwatch stopwatch = new Stopwatch();
+
+			double singleThreadedTime = 0;
+			for (int i = 0; i < numberOfTests; i++)
+            {
+				stopwatch.Restart();
+				int count = matrix.CountSatisfyCondition(condition);
+				stopwatch.Stop();
+				singleThreadedTime += stopwatch.ElapsedMilliseconds;
+			}
+			singleThreadedTime /= numberOfTests;
+			Console.WriteLine($"Single-threaded average execution time: {singleThreadedTime} ms");
+
+
+			double multiThreadedTime = 0;
+			for (int i = 0; i < numberOfTests; i++)
+			{
+				stopwatch.Restart();
+				int countMultithreaded = matrix.ThreadCountSatisfyCondition(condition, blockSize, threadCount);
+				stopwatch.Stop();
+				multiThreadedTime += stopwatch.ElapsedMilliseconds;
+			}
+			multiThreadedTime /= numberOfTests;
+			Console.WriteLine($"Multi-threaded average execution time: {multiThreadedTime} ms");
+
+
+			double speedup = (double)singleThreadedTime / multiThreadedTime;
+			Console.WriteLine($"Speedup: {speedup:F2}x\n");
+		}
+	}
+}
